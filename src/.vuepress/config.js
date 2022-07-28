74259620e026ab84a0700ce379411198b7dc6891
Sergey Zhuravlev <szhuravlev@mtt.ru>
2022-07-28 22:05:06 +0300

    feat: add new files and content

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -65,11 +65,13 @@ module.exports = {
           children: [
             'start/1',
             'start/2',
+            'start/8',
             'start/3',
             'start/4',
-            'start/5',
+            'start/9',
             'start/6',
-            'start/7'
+            'start/7',
+            'start/5'
           ]
         },
         {
